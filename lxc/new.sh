5ad5191a30f8bb48e621c1d02d278f7bf9c226d9
Alexandr Alakin <alakinalexandr@gmail.com>
2017-01-08 16:30:26 +0600

    Name of new VM can be passed as an argument

--- lxc/new.sh
+++ lxc/new.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
+# Creates a new LXC VM
+# Name of new VM can be passed as an argument or entered interactively
+
+# Links below were worth reading :)
 # http://www.nefigtut.ru/2015/11/18/libvirt-lxc-virtualization-1/
 # https://der-linux-admin.de/2014/08/centos-7-centos-7-im-lxc-container/
 # https://wiki.centos.org/HowTos/LXC-on-CentOS6
 # https://access.redhat.com/articles/1365153
 # https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/7/html/Virtualization_Deployment_and_Administration_Guide/sect-Guest_virtual_machine_installation_overview-Creating_guests_with_virt_install.html
-
-virsh -c lxc:/// list --all
-read -p "Enter LXC VM Name to create: " LXC_NAME
-if virsh -c lxc:/// domstate ${LXC_NAME} &>/dev/null; then echo "VM already exists! Exiting..."; exit; fi
 
 #LXC_NAME=c2
 LXC_RAM=512
@@ -15,6 +15,15 @@ LXC_REL=7
 LXC_OSV=centos7.0
 LXC_NET=default
 LXC_PKG="systemd passwd yum centos-release vim-enhanced openssh-server procps-ng iproute net-tools dhclient sudo rootfiles tcpdump"
+
+if [ "$#" -gt 0 ]; then
+    LXC_NAME=${1}
+else
+    virsh -c lxc:/// list --all
+    read -p "Enter LXC VM Name to create: " LXC_NAME
+fi
+
+if virsh -c lxc:/// domstate ${LXC_NAME} &>/dev/null; then echo "VM already exists! Exiting..."; exit; fi
 
 ROOT_FS=/var/lib/libvirt/lxc/
 LXC_RFS=${ROOT_FS}${LXC_NAME}/
